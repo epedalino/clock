012f7e8c639aa82e3cac4bb9647c1b90b7859b12
epedalino <epedalino@gmail.com>
2020-03-24 19:25:59 -0400

    useRef implementation in progress. Seconds degrees are logged but clock does not display the movement

--- src/Clock.js
+++ src/Clock.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Clock.css';
 
-// const minsHand = document.querySelector('.min-hand');
-// const secondHand = document.querySelector('.second-hand');
-// const hourHand = document.querySelector('.hour-hand');
-
-//   function setDate() {
-//     const now = new Date();
-
-//     const seconds = now.getSeconds();
-//     const secondsDegrees = ((seconds / 60) * 360) + 90;
-//     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-//     const mins = now.getMinutes();
-//     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-//     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-//     const hour = now.getHours();
-//     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-//     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-//   }
-//   setInterval(setDate, 1000);
-//   setDate();
-
 const Clock = () => {
+  
+  var secondsHand = useRef(null);
+  //var minsHand = useRef(null);
+  //var hourHand = useRef(null);
+  
+  function setDate () {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    
+    secondsHand.current = `rotate(${secondsDegrees}deg)`;
+    console.log("seconds hand", secondsHand);
+
+  }
+   setInterval(setDate, 1000);
+   setDate();
+
   return (
-    <div class="clock">
-      <div class="clock-face">
-        <div class="hand hour-hand"></div>
-        <div class="hand min-hand"></div>
-        <div class="hand second-hand"></div>
+    <div className="clock">
+      <div className="clock-face">
+        <div className="hand hour-hand"></div>
+        <div className="hand min-hand"></div>
+        <div className="hand second-hand" ref={ secondsHand }></div>
       </div>
     </div>
   )
-}
+ }
 export default Clock;
